99d1e879009ad7780a5bf71ef98c33b349a2f69e
Captain <diana.iminza@ronforddigital.com>
2024-09-04 16:41:55 +0300

    Add project files.

---
+++ TaskManagementApi/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagementApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; } // Store hashed passwords
+        public ICollection<TaskData> Tasks { get; set; }
+    }
+}
